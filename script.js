0c706868fe6731bf014eb06e9f3dcc3ea89f5195
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-05-27 15:48:36 +0200

    got the computers input

---
+++ script.js
@@ -0,0 +1,10 @@
+let options = ["rock", "paper", "scissors"];
+// console.log(options);
+
+//getting the PC's input
+function getComputerInput() {
+    let computerInput = options[Math.floor(Math.random() * 3)];
+     console.log(computerInput);
+};
+
+//getting the player's input
