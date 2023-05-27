a9fb343090c6f31b8d5ea56d3267a03630489840
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-05-27 18:22:02 +0200

    creating conditions for the wins and loses

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let options = ["rock", "paper", "scissors"];
+let roundWin = ``;
 // console.log(options);
 
 //getting the PC's input
 function getComputerInput() {
     let computerInput = options[Math.floor(Math.random() * 3)];
-     console.log(computerInput);
+     console.log(`Computer picked: ${computerInput}`);
 };
+
+let computerChoice = getComputerInput();
 
 //getting the player's input
 function getPlayersInput() {
-    let playersInput = prompt("Rock, paper or scissors?");
+    let playersInput = prompt(`Rock, paper or scissors?`);
     playersInput = playersInput.toLowerCase();
-    if (playersInput === 'paper' || playersInput === 'rock' || playersInput === 'scissors') {
+    if (playersInput === `paper` || playersInput === `rock` || playersInput === `scissors`) {
         console.log(`You picked: ${playersInput.toLowerCase()}`);
     } else {
         prompt(`Error: please enter a correct value.`)
@@ -19,10 +22,29 @@ function getPlayersInput() {
     
 };
 
-//playing the game
-function playingGame(playersChoice, computerChoice) {
-    let playersChoice = getPlayersInput();
-    console.log(playersChoice);
-    let computerChoice = getComputerInput();
-    console.log(computerChoice);
+let playersChoice = getPlayersInput();
+
+//playing a single round
+function playRound(playersChoice, computerChoice) {
+    if (
+        (playersChoice === `rock` && computerChoice === `scissors`) ||
+        (playersChoice === `scissors` && computerChoice === `paper`) ||
+        (playersChoice === `paper` && computerChoice === `rock`)
+  ) { 
+        console.log(roundWin = `player`);
+    };
+
+    if (
+        (computerChoice === `rock` && playersChoice === `scissors`) ||
+        (computerChoice === `scissors` && playersChoice === `paper`) ||
+        (computerChoice === `paper` && playersChoice === `rock`)
+    ) {
+        console.log(roundWin = `computer`);
+    };
+
+
 };
+
+
+
+console.log(playRound(playersChoice, computerChoice));
